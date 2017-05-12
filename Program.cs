ec3b4d739ddb203b89a5dcf8cbdfdc68eee2a75f
WIN_NT\x16j028 <x16j028@chiba-fjb.ac.jp>
2017-05-12 11:40:12 +0900

    hh

--- Program.cs
+++ Program.cs
@@ -10,6 +10,21 @@ namespace Example6
     {
         static void Main(string[] args)
         {
+            //変数の宣言
+            int score;
+            double pai;
+            string name;
+
+            //変数への値の代入
+            score = 25;
+            pai = 3.1415;
+            name = "TARO";
+
+            //変数に格納された値を画面に表示
+            Console.WriteLine("SCORE = {0}", score);
+            Console.WriteLine("AVE   = {0}", pai);
+            Console.WriteLine("NAME  = {0}", name);
+
         }
     }
 }
